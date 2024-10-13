2dea236f691e72c0e1bb30b00f2d360e1b088f94
Daniel Lopes <ddanielssoares@gmail.com>
2024-10-13 01:23:57 -0300

    Exemplo transform

--- geometric_shapes_transform.cpp
+++ geometric_shapes_transform.cpp
@@ -2,7 +2,10 @@
 
 void display() {
     glClear(GL_COLOR_BUFFER_BIT);
-
+    
+    glPushMatrix();
+    glTranslatef(-0.5, -0.5, 0.0);
+    
     glBegin(GL_TRIANGLES);
         glColor3f(1.0, 0.0, 0.0);
         glVertex2f(-0.5, -0.5);
@@ -11,7 +14,9 @@ void display() {
         glColor3f(0.0, 0.0, 1.0);
         glVertex2f(0.0, 0.5);
     glEnd();
-
+    
+    glPopMatrix();
+    
     glBegin(GL_QUADS);
         glColor3f(1.0, 1.0, 0.0);
         glVertex2f(-0.5, 0.5);
@@ -22,6 +27,7 @@ void display() {
 
     glFlush();
 }
+
 
 void init() {
     glClearColor(0.0, 0.0, 0.0, 0.0);
