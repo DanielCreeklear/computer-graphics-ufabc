56191aa51e392f1e7875f8a2fc4483d730acf92b
Daniel Lopes <ddanielssoares@gmail.com>
2024-10-13 01:21:01 -0300

    Formas geométricas

---
+++ geometric_shapes.cpp
@@ -0,0 +1,40 @@
+#include <GL/glut.h>
+
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glBegin(GL_TRIANGLES);
+        glColor3f(1.0, 0.0, 0.0);
+        glVertex2f(-0.5, -0.5);
+        glColor3f(0.0, 1.0, 0.0);
+        glVertex2f(0.5, -0.5);
+        glColor3f(0.0, 0.0, 1.0);
+        glVertex2f(0.0, 0.5);
+    glEnd();
+
+    glBegin(GL_QUADS);
+        glColor3f(1.0, 1.0, 0.0);
+        glVertex2f(-0.5, 0.5);
+        glVertex2f(0.5, 0.5);
+        glVertex2f(0.5, 1.5);
+        glVertex2f(-0.5, 1.5);
+    glEnd();
+
+    glFlush();
+}
+
+void init() {
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glColor3f(1.0, 1.0, 1.0);
+}
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE);
+    glutInitWindowSize(500, 500);
+    glutCreateWindow("OpenGL - Formas Geométricas");
+    init();
+    glutDisplayFunc(display);
+    glutMainLoop();
+    return 0;
+}
