e96b1119f92bff7307594ab9c1b9e138058f01f8
Daniel Lopes <ddanielssoares@gmail.com>
2024-10-13 01:16:40 -0300

    Correção identação

--- hello_world.cpp
+++ hello_world.cpp
@@ -4,11 +4,12 @@ void display() {
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
     glColor3f(1.0f, 0.0f, 0.0f);
+    
     glBegin(GL_QUADS);
-    glVertex2f(-0.5f, -0.5f);
-    glVertex2f( 0.5f, -0.5f);
-    glVertex2f( 0.5f, 0.5f);
-    glVertex2f(-0.5f, 0.5f);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f( 0.5f, -0.5f);
+        glVertex2f( 0.5f, 0.5f);
+        glVertex2f(-0.5f, 0.5f);
     glEnd();
     glFlush();
 }
